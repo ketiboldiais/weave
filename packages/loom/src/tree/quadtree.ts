ad5c941bbdeef21472913c1be19f5663e0a98fbd
Ketib Oldiais <numerstack@gmail.com>
2023-07-05 02:33:43 -0500

    finish graph layout 2

--- packages/loom/src/tree/quadtree.ts
+++ packages/loom/src/tree/quadtree.ts
@@ -1,8 +1,41 @@
-import { v2 } from "@weave/math";
+import { v2, Vector } from "@weave/math";
 
-const vs = [
-  v2(1, 1),
-  v2(1, 3),
-  v2(4, 5),
-  v2(-8, 1),
-];
+class Quad {
+  constructor(
+    public x: number,
+    public y: number,
+    public w: number,
+    public h: number,
+  ) {
+  }
+}
+
+const quad = (x: number, y: number, w: number, h: number) => (
+  new Quad(x, y, w, h)
+);
+
+class QuadTree {
+  points: Vector[] = [];
+  I: QuadTree | null = null;
+  II: QuadTree | null = null;
+  III: QuadTree | null = null;
+  IV: QuadTree | null = null;
+  constructor(
+    public boundary: Quad,
+    public capacity: number,
+  ) {}
+  subdivide() {
+		
+  }
+  push(point: Vector) {
+    if (this.points.length < this.capacity) {
+      this.points.push(point);
+    } else {
+      this.subdivide();
+    }
+  }
+}
+
+const quadtree = (boundary: Quad, capacity: number) => (
+  new QuadTree(boundary, capacity)
+);
